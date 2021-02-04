7b16aded8aed6dc701a9eddba7ad79f635c81540
YuraTadevosyan <yuratadevosyan01@gmail.com>
2021-02-04 17:26:52 +0400

    final change

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -18,27 +18,3 @@ screenSize.addEventListener('change', (e) => {
         burger_icon.setAttribute('class', 'burger')
     }
 });
-
-
-/* Change pricing plan button color */
-// const planButtonPlus = document.querySelector('.plus .plan_button');
-// const planButtonPremium = document.querySelector('.premium .plan_button');
-// const screenSize = window.matchMedia('(min-width: 799px) and (max-width: 1035px)');
-//
-// function changeColor(e) {
-//     if (e.matches){
-//         planButtonPlus.style.backgroundColor = '#83a7a5';
-//         planButtonPremium.style.backgroundColor = '#6fb048';
-//
-//         planButtonPlus.setAttribute('class','plan_button effect effect_green');
-//         planButtonPremium.setAttribute('class','plan_button effect effect_gray');
-//     }else{
-//         planButtonPlus.style.backgroundColor = '#6fb048';
-//         planButtonPremium.style.backgroundColor = '#83a7a5';
-//
-//         planButtonPlus.setAttribute('class','plan_button effect effect_gray');
-//         planButtonPremium.setAttribute('class','plan_button effect effect_green');
-//     }
-// }
-//
-// screenSize.addEventListener('change',changeColor)
